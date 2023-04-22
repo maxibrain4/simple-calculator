0c4a80f727269ae00fe2b8819a14da80b21b7c91
Fakunle Israel <maxibrain4@gmail.com>
2023-04-22 21:45:44 +0100

    all buttons are working

--- index.js
+++ index.js
@@ -1,72 +1,69 @@
-console.log('hello');
-/* <form name="form">
-      <div class="one">
-        <input type="text" id="men" name="screen" /><br /><br />
-        <button type="button" id="btn" onclick="form.screen.value += '1'">
-          1
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '2'">
-          2
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '3'">
-          3
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '4'">
-          4</button
-        ><br />
-        <button type="button" id="btn" onclick="form.screen.value += '5'">
-          5
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '6'">
-          6
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '7'">
-          7
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '8'">
-          8</button
-        ><br />
-        <button type="button" id="btn" onclick="form.screen.value += '9'">
-          9
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '0'">
-          0
-        </button>
-        <button
-          type="button"
-          id="btn"
-          onclick="form.screen.value = form.screen.value.slice(0,-1)"
-        >
-          ←
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '+'">
-          +</button
-        ><br />
-        <button type="button" id="btn" onclick="form.screen.value += '-'">
-          -
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '*'">
-          *
-        </button>
-        <button type="button" id="btn" onclick="form.screen.value += '/'">
-          /
-        </button>
-        <button
-          type="button"
-          id="btn"
-          onclick="form.screen.value = Math.sqrt(form.screen.value)"
-        >
-          √
-        </button>
-        <button
-          type="button"
-          id="btn"
-          onclick="form.screen.value = eval(form.screen.value)"
-        >
-          =
-        </button>
-        <button type="button" id="clear" onclick="form.screen.value = '' ">
-          Clear All
-        </button>
-      </div>
-    </form> */
+const allBtn = document.querySelectorAll('.btn--click');
+const del = document.querySelector('#del-btn');
+const reset = document.querySelector('#reset-btn');
+const equals = document.querySelector('#equals-btn');
+const plus = document.querySelector('#add');
+const minus = document.querySelector('#minus');
+const multiply = document.querySelector('#multiply');
+const divide = document.querySelector('#divide');
+const switchOne = document.querySelector('#on');
+const switchTwo = document.querySelector('#off');
+const switchThree = document.querySelector('#na');
+let screen = document.querySelector('.screen');
+let screenValue = '';
+
+// new Intl.NumberFormat(navigator.language).format(screen.value);
+allBtn.forEach(function (el) {
+  el.addEventListener('click', function () {
+    const id = this.getAttribute('data-num');
+    screenValue += id;
+    screen.value = screenValue;
+  });
+});
+del.addEventListener('click', function () {
+  screen.value = screen.value.slice(0, -1);
+  if (screen.value == '') {
+    screenValue = '';
+  }
+});
+plus.addEventListener('click', function () {
+  const id = this.getAttribute('data-num');
+  screenValue += id;
+  screen.value = screenValue;
+});
+equals.addEventListener('click', function () {
+  screen.value = eval(screen.value);
+});
+reset.addEventListener('click', function () {
+  screen.value = '';
+  screenValue = '';
+});
+
+minus.addEventListener('click', function () {
+  const id = this.getAttribute('data-num');
+  screenValue += id;
+  screen.value = screenValue;
+});
+multiply.addEventListener('click', function () {
+  const id = this.getAttribute('data-num');
+  screenValue += id;
+  screen.value = screenValue;
+});
+divide.addEventListener('click', function () {
+  const id = this.getAttribute('data-num');
+  screenValue += id;
+  screen.value = screenValue;
+});
+const container = document.querySelector('.container');
+const btnBody = document.querySelector('.btn-body');
+switchTwo.addEventListener('click', function () {
+  container.style.backgroundColor = 'hsl(0, 0%, 90%)';
+  btnBody.style.backgroundColor = 'hsl(0, 5%, 81%)';
+  screen.style.backgroundColor = 'hsl(0, 0%, 93%)';
+  screen.style.color = 'hsl(60, 10%, 19%)';
+  del.style.backgroundColor = 'hsl(185, 42%, 37%)';
+  reset.style.backgroundColor = 'hsl(185, 42%, 37%)';
+  equals.style.backgroundColor = 'hsl(25, 98%, 40%)';
+  switchTwo.style.backgroundColor = 'hsl(25, 98%, 40%)';
+  allBtn.style.color = 'hsl(60, 10%, 19%)';
+});
