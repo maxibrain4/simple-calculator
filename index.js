94e0a5d340922c37db106610f32d85d5417767d3
Fakunle Israel <maxibrain4@gmail.com>
2023-04-24 21:50:55 +0100

    all toggle buttons working

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ const divide = document.querySelector('#divide');
 const switchOne = document.querySelector('#on');
 const switchTwo = document.querySelector('#off');
 const switchThree = document.querySelector('#na');
+const btn = document.querySelector('.btncolor');
 let screen = document.querySelector('.screen');
-let screenValue = '';
+const container = document.querySelector('.container');
+const btnBody = document.querySelector('.btn-body');
 
+const description = document.querySelector('.description ');
+let screenValue = '';
 // new Intl.NumberFormat(navigator.language).format(screen.value);
 allBtn.forEach(function (el) {
   el.addEventListener('click', function () {
@@ -21,10 +25,8 @@ allBtn.forEach(function (el) {
   });
 });
 del.addEventListener('click', function () {
-  screen.value = screen.value.slice(0, -1);
-  if (screen.value == '') {
-    screenValue = '';
-  }
+  screenValue = screen.value.slice(0, -1);
+  screen.value = screenValue;
 });
 plus.addEventListener('click', function () {
   const id = this.getAttribute('data-num');
@@ -32,7 +34,8 @@ plus.addEventListener('click', function () {
   screen.value = screenValue;
 });
 equals.addEventListener('click', function () {
-  screen.value = eval(screen.value);
+  screenValue = eval(screen.value);
+  screen.value = screenValue;
 });
 reset.addEventListener('click', function () {
   screen.value = '';
@@ -54,9 +57,19 @@ divide.addEventListener('click', function () {
   screenValue += id;
   screen.value = screenValue;
 });
-const container = document.querySelector('.container');
-const btnBody = document.querySelector('.btn-body');
-switchTwo.addEventListener('click', function () {
+
+switchOne.addEventListener('click', function () {
+  container.style.backgroundColor = '';
+  btnBody.style.backgroundColor = '';
+  screen.style.backgroundColor = '';
+  screen.style.color = '';
+  del.style.backgroundColor = '';
+  reset.style.backgroundColor = '';
+  equals.style.backgroundColor = '';
+  description.style.color = '';
+  // allBtn.style.color = '';
+});
+switchTwo.addEventListener('click', function (e) {
   container.style.backgroundColor = 'hsl(0, 0%, 90%)';
   btnBody.style.backgroundColor = 'hsl(0, 5%, 81%)';
   screen.style.backgroundColor = 'hsl(0, 0%, 93%)';
@@ -64,6 +77,21 @@ switchTwo.addEventListener('click', function () {
   del.style.backgroundColor = 'hsl(185, 42%, 37%)';
   reset.style.backgroundColor = 'hsl(185, 42%, 37%)';
   equals.style.backgroundColor = 'hsl(25, 98%, 40%)';
-  switchTwo.style.backgroundColor = 'hsl(25, 98%, 40%)';
-  allBtn.style.color = 'hsl(60, 10%, 19%)';
+  description.style.color = 'hsl(60, 10%, 19%)';
+  // allBtn.style.color = 'hsl(60, 10%, 19%)';
+  // switchtoggle.backgroundColor = 'green';
+  // e.target.firstChild.add('btnColor');
+  // btn.style.backgroundColor = 'green';
+});
+switchThree.addEventListener('click', function () {
+  container.style.backgroundColor = 'hsl(268, 75%, 9%)';
+  btnBody.style.backgroundColor = 'hsl(268, 71%, 12%)';
+  screen.style.backgroundColor = 'hsl(268, 71%, 12%)';
+  screen.style.color = 'hsl(52, 100%, 62%)';
+  del.style.backgroundColor = 'hsl(281, 89%, 26%)';
+  reset.style.backgroundColor = 'hsl(281, 89%, 26%)';
+  equals.style.backgroundColor = 'hsl(176, 100%, 44%)';
+  description.style.color = 'hsl(52, 100%, 62%)';
+  // allBtn.classList.add('.btnColor');
+  btn.style.backgroundColor = 'red';
 });
